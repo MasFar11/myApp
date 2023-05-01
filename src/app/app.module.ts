011af53f64ea869326321353081f037949f0d88c
Masoumeh Farokhnia <mfrkhna@gmail.com>
2023-05-01 20:58:52 +0200

    initial commit first pr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FormGroup, ReactiveFormsModule }  from '@angular/forms';
+import { loginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: loginComponent },
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    TopBarComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
