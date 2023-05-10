a94e73194200fd482dcc160198cc53a1023ca7a3
Masoumeh Farokhnia <mfrkhna@gmail.com>
2023-05-10 15:09:25 +0200

    add json server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule }  from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MatIconModule } from '@angular/material/icon';
-import { FormGroup, ReactiveFormsModule }  from '@angular/forms';
 import { loginComponent } from './login/login.component';
-import { RouterModule } from '@angular/router';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    MatIconModule,
+    MatButtonModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: loginComponent },
